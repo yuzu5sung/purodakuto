8510f2c7b408160ed32f6aa6b223f2a39fa48dbc
yuzu <yuzukif0615@gmail.ac.jp>
2026-09-15 05:31:11 +0900

    message

--- Happy Hack/js/main.js
+++ Happy Hack/js/main.js
@@ -1,16 +1,49 @@
-$(function($){
-    $('.firstview').bgSwitcher({
-        images:['images/background1.jpg','images/background2.jpg','images/background3.jpg','images/background4.jpg','images/background5.jpg'],
-        interval:3000,
-        loop:true,
-    });
+$(function ($) {
+  $(".firstview").bgSwitcher({
+    images: [
+      "images/background1.jpg",
+      "images/background2.jpg",
+      "images/background3.jpg",
+      "images/background4.jpg",
+      "images/background5.jpg",
+    ],
+    interval: 3000,
+    loop: true,
+  });
 
-    $('.slide-items').slick({
-        autoplay:true,
-        slidesToShow:4,
-        slidesToScroll:1,
-        arrows:true,
-        dots:true,
-       
-    });
+  $(".slide-items").slick({
+    autoplay: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  particlesJS("particles-js", {
+    particles: {
+      number: { value: 70 },
+      size: { value: 1 },
+      move: { speed: 7 },
+    },
+  });
 });
